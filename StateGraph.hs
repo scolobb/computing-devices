34784d9585b93edba3b33796cb421c54d5928734
Sergiu Ivanov <unlimitedscolobb@gmail.com>
2013-12-13 08:41:41 +0100

    Start the module StateGraph.

---
+++ StateGraph.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE DeriveGeneric #-}
+{- StateGraph.hs
+
+A draft solution for manipulating state graphs.  A state graph is a
+computing device consisting of a some states arranged in a graph
+(suprise) whose edges are labelled with conditions and operations.-}
+
+module StateGraph () where
