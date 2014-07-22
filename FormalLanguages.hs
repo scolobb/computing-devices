d93fd75fe7d5df1961eb776da82b9e4c53021850
Sergiu Ivanov <sivanov@colimite.fr>
2014-07-22 13:38:37 +0300

    Add Symbol, Word, and Language to FormalLanguages.

--- FormalLanguages.hs
+++ FormalLanguages.hs
@@ -2,4 +2,13 @@
 
 Defines some basic notions of the formal language theory.-}
 
-module FormalLanguages () where
+module FormalLanguages ( Symbol
+                       , Word
+                       , Language
+                       ) where
+
+import qualified Data.Set as Set
+
+type Symbol = String
+type Word = [Symbol]
+type Language = Set.Set Word
