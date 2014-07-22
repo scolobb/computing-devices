b7baeaf7123a8fd285d04502364e9622816448e7
Sergiu Ivanov <sivanov@colimite.fr>
2014-07-22 13:58:39 +0300

    Add some basic definitions to TuringMachine.

--- TuringMachine.hs
+++ TuringMachine.hs
@@ -3,4 +3,30 @@
 Contains the definition of the Turing machine as well as a description
 of a universal Turing machine by Yu. Rogozhin.-}
 
-module TuringMachines () where
+module TuringMachines ( State
+                      , MoveDirection
+                      , TransitionFunc
+                      , TuringMachine
+                      ) where
+
+import FormalLanguages
+import qualified Data.Map as Map
+
+type State = Int
+
+data MoveDirection = MoveLeft | MoveRight
+                   deriving (Show, Read, Eq, Ord)
+
+-- | The transition function of the Turing machine.
+type TransitionFunc = Map.Map (State, Symbol) (Symbol, MoveDirection, State)
+
+-- | The Turing machine.
+--
+-- The input alphabet is not given explicitly.
+data TuringMachine = TuringMachine
+                     [State]          -- ^ The states of the Turing machine.
+                     [Symbol]         -- ^ The alphabet of symbols.
+                     Symbol           -- ^ The blank symbol.
+                     State            -- ^ The initial state.
+                     TransitionFunc   -- ^ The transition function.
+                   deriving (Show, Read, Eq, Ord)
