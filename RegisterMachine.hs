b299ede2f82f2e82894870ddb4d8cb99d473462b
Sergiu Ivanov <sivanov@colimite.fr>
2014-10-10 14:55:42 +0200

    Add the weakly universal register machine U_{19}.

--- RegisterMachine.hs
+++ RegisterMachine.hs
@@ -9,6 +9,7 @@ module RegisterMachine ( State
                        , Program
                        , RegisterMachine(..)
                        , u22
+                       , u19
                        , registerUse
                        ) where
 
@@ -65,6 +66,42 @@ u22 = RegisterMachine [0..7] $ IntMap.fromList
       , ( 29,   RiP  0    1    )
       , ( 30,   RiP  2   31    )
       , ( 31,   RiP  3   32    )
+      , ( 32,   RiZM 4    1 34 )
+
+      , ( 34,   HALT           )
+      ]
+
+-- | A (weakly) universal register machine with 19 instructions of
+-- types /RiP/ and /RiZM/.
+--
+-- Ivan Korec: Small Universal Register
+-- Machines. Theor. Comput. Sci. 168(2): 267-301 (1996).
+u19 :: RegisterMachine
+u19 = RegisterMachine [0,1,2,4,5,6,7] $ IntMap.fromList
+      -- Instruction reader.
+      [ (  1,   RiZM 1    3  6 )
+      , (  3,   RiP  7    1    )
+      , (  4,   RiZM 5    6  7 )
+      , (  6,   RiP  6    4    )
+      , (  7,   RiZM 6    9  4 )
+      , (  9,   RiP  5   10    )
+      , ( 10,   RiZM 7   12 13 )
+      , ( 12,   RiP  1    7    )
+      , ( 13,   RiZM 6   33  1 )
+      , ( 33,   RiP  6   14    )
+      , ( 14,   RiZM 4    1 16 )
+
+        -- Decoder.
+      , ( 16,   RiZM 5   18 23 )
+      , ( 18,   RiZM 5   20 27 )
+      , ( 20,   RiZM 5   22 30 )
+      , ( 22,   RiP  4   16    )
+
+        -- Simulation block.
+      , ( 23,   RiZM 0   32  1 )
+      , ( 27,   RiZM 2   32  1 )
+      , ( 30,   RiP  0   31    )
+      , ( 31,   RiP  2   32    )
       , ( 32,   RiZM 4    1 34 )
 
       , ( 34,   HALT           )
